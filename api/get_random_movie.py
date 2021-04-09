0d82c5a654acdfdfa69f1795a9033c33be444965
Michał Szóstak <mszostakk01@gmail.com>
2021-04-09 16:40:38 +0200

    redesing search function

--- api/get_random_movie.py
+++ api/get_random_movie.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response, Response
 from flask_restful import Resource, Api
 
 from services.random_movie import get_random_movie
+import traceback
 
 class GetRandomMovie(Resource):
   def post(self):
@@ -12,7 +13,7 @@ class GetRandomMovie(Resource):
       start_year = body['startYear']
       end_year = body['endYear']
       genres_arr = genres.split(', ')
-      return get_random_movie(genres_arr, startYear, endYear)
+      return get_random_movie(genres_arr, start_year, end_year)
     except:
-      print('except')
+      traceback.print_exc()
       return get_random_movie([])
--- services/random_movie.py
+++ services/random_movie.py
@@ -6,20 +6,12 @@ from services.categories import get_categories_parsed
 def get_random_movie(genres, start_year = 1888, end_year = 2024):
   max_page = 1000
   genres = get_categories_parsed(genres)
-  rand_page_num = random.randint(1,max_page)
-  url = f'https://www.filmweb.pl/films/search?genres={genres}&orderBy=popularity&descending=true&startYear={start_year}&endYear={end_year}&page={rand_page_num}'
-  print(url)
-  res = requests.get(url)
-  
-
-  i = 2
-  while(res.status_code == 404):
-    rand_page_num = random.randint(1, max_page/i)
-    res = requests.get(url),
-    i += 2
+  rand_page_num = random.randint(1, 20)
+  url = f'https://www.filmweb.pl/films/search?genres={genres}&orderBy=popularity&descending=true&startYear={start_year}&endYear={end_year}'
+  print(url + f'&page={rand_page_num}')
+  res = requests.get(url + f'&page={rand_page_num}')
 
   soup = bs(res.content, features="lxml")
-
   movies = soup.select('.hits__item')
   movie = random.choice(movies)
 
